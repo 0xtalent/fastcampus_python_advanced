0c48c5eb8a0e9c59693cef1a0ab820bfc7f344d2
marinehayong <marinehayong@gmail.com>
2019-10-20 10:26:50 +0900

    객체지향 프로그래밍(OOP)에 대해서 맛보기 해봤다. 더 열심히 공부해야 합니다 어려워요

--- chapter01_01.py
+++ chapter01_01.py
@@ -88,10 +88,33 @@ class Student():
         self._details = details
 
     def __str__(self):
-        return 'str : {}'.format(self._name)
+        return 'str : {} - {}'.format(self._name, self._number)
+
+    def __repr__(self):
+        return 'repr : {} - {}'.format(self._name, self._number)
 
 student1 = Student('kim', 1, 1, {'gender': 'Male', 'score1' : 95, 'score2' : 88})
 student2 = Student('Lee', 2, 2, {'gender': 'Female', 'score1' : 77, 'score2' : 92})
 student3 = Student('Park', 3, 4, {'gender': 'Male', 'score1' : 99, 'score2' : 100})
 
 print(student1.__dict__)
+
+# 리스트 선언
+students_list = []
+
+students_list.append(student1)
+students_list.append(student2)
+students_list.append(student3)
+
+print()
+
+print(students_list)
+
+# 반복(__str__)
+for x in students_list:
+    print(x)
+
+# 객체지향의 정의는 실제 세계에 존재하는 학생을 직접 모델링했죠. Class로
+# 모델링을 해서, 모델링을 했다는 것은 그 학생이라는 객체, 삶, 실제 세계에 있는 학생을
+# 어떤 가진 속성들을 이렇게 모델링해서 프로그램을 개발하는 과정을 객체지향 프로그래밍이라 한다.
+# 어렵다...더 공부해야겠다!
